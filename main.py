ac442cc58c5a3b0bbe48e6115c9706bc49d48dfc
jerrychen99 <66809252+jerrychen99@users.noreply.github.com>
2025-07-18 11:06:59 +0800

    Update main.py

--- main.py
+++ main.py
@@ -33,8 +33,8 @@ def predict():
 
     return jsonify({
         "food": food_name,
-        "calories": str(info["calories"]),
-        "protein": str(info["protein"])
+        "calories": info["calories"],
+        "protein": info["protein"]
     })
 
 if __name__ == "__main__":
